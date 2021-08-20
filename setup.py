5d6f0af1a572b80b3b74cd83b20fdf7202844a7b
ziserendexin <32018376+ziserendexin@users.noreply.github.com>
2021-08-20 14:26:30 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: HeathKang
+Date: 2017-12-22 16:38:59
+LastEditors: Zhang Hengye
+LastEditTime: 2021-08-20 14:26:13
+'''
 from setuptools import setup, find_packages
 
 setup(name="simplecannet",
       url="https://github.com/HeathKang/simplecannet",
-      version="0.0.1",
+      version="0.0.2",
       author="Heath Kang",
       author_email="heath.kang@foxmail.com",
       packages=find_packages(),
